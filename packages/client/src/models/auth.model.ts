c6913b464182dca4ee18b38f73c15d9a7f0d1f3b
Anton Rodionov <48121921+stvdent47@users.noreply.github.com>
2023-03-29 10:57:25 +0400

    WIZ-26: adds register page layout (with no logics) (#5)

--- packages/client/src/models/auth.model.ts
+++ packages/client/src/models/auth.model.ts
@@ -13,6 +13,33 @@ export class LoginInputDto {
   }
 }
 
+export type RegisterInput = {
+  firstName: string;
+  secondName: string;
+  login: string;
+  email: string;
+  password: string;
+  phone: string;
+};
+
+export class RegisterInputDto {
+  first_name: string;
+  second_name: string;
+  login: string;
+  email: string;
+  password: string;
+  phone: string;
+
+  constructor(info: RegisterInput) {
+    this.first_name = info.firstName;
+    this.second_name = info.secondName;
+    this.login = info.login;
+    this.email = info.email;
+    this.password = info.password;
+    this.phone = info.phone;
+  }
+}
+
 export type CurrentUser = {
   id: string;
 };
