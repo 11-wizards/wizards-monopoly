f80b75dd6ad223556dc3aefea97716492454fc91
Pavel Bologan <p.bologan@gmail.com>
2023-04-01 19:48:08 +0400

    moves useAppDispatch and useAppSelector from store.ts to hooks.ts

--- packages/client/src/app/store.ts
+++ packages/client/src/app/store.ts
@@ -1,6 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import type { TypedUseSelectorHook } from 'react-redux';
-import { useDispatch, useSelector } from 'react-redux';
 import userReducer from './slices/userSlice';
 import gameSettingsReducer from './slices/gameSettingsSlice';
 
@@ -14,5 +12,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
