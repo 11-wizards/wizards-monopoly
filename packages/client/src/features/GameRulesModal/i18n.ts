15095b17cc888851ac36f6b33e632a2fa2aea365
Pavel Bologan <p.bologan@gmail.com>
2023-04-01 19:46:42 +0400

    separates translation messages for LandingPage and GameRulesModal

---
+++ packages/client/src/features/GameRulesModal/i18n.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  gameRulesTitle: {
+    id: 'rules.title',
+    defaultMessage: 'Game rules',
+  },
+  gameRulesCloseButton: {
+    id: 'rules.close.button',
+    defaultMessage: 'Close',
+  },
+});
