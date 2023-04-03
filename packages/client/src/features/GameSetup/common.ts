78e4fd0c32930635cebcb8eb0c841f3cac7a6e84
rnuravtsev <rnuravtsev@gmail.com>
2023-04-03 10:24:01 +0300

    feat:(WIZ-31) adds defaultMessages

--- packages/client/src/features/GameSetup/common.ts
+++ packages/client/src/features/GameSetup/common.ts
@@ -2,16 +2,30 @@ import type { GameSetupFormData } from 'features/GameSetup/types';
 import { defineMessages } from 'react-intl';
 
 export const messages = defineMessages({
-  buttonStart: { id: 'start.button.start' },
-
-  textChoose: { id: 'start.text.choose' },
-  playerName: { id: 'start.player.name' },
-  playerColor: { id: 'start.player.color' },
-  errorRequired: { id: 'validation.required-field' },
-  errorMinLength: { id: 'validation.min-length.player_name' },
-  errorMaxLength: { id: 'validation.max-length.player_name' },
-  errorPattern: { id: 'validation.pattern.player_name' },
-  errorColorsUnique: { id: 'validation.unique.color' },
+  buttonStart: { id: 'start.button.start', defaultMessage: 'Start game' },
+  textChoose: { id: 'start.text.choose', defaultMessage: 'Choose the number of players:' },
+  playerName: {
+    id: 'start.player.name',
+    defaultMessage: 'Choose the name of the {num, number}nd player:',
+  },
+  playerColor: {
+    id: 'start.player.color',
+    defaultMessage: 'Choose the color of the {num, number}nd player:',
+  },
+  errorRequired: { id: 'validation.required-field', defaultMessage: 'This field is required' },
+  errorMinLength: {
+    id: 'validation.min-length.player_name',
+    defaultMessage: 'Player name must be at least {min, number} character',
+  },
+  errorMaxLength: {
+    id: 'validation.max-length.player_name',
+    defaultMessage: 'Player name cannot be longer than {max, number} characters',
+  },
+  errorPattern: {
+    id: 'validation.pattern.player_name',
+    defaultMessage: 'Player name can only contain letters and numbers',
+  },
+  errorColorsUnique: { id: 'validation.unique.color', defaultMessage: 'Colors must be unique' },
 });
 
 export const createPlayersArray = (length: Nullable<number>) =>
