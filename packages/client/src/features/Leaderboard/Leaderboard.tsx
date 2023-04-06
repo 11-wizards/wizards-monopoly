516cdf5cb544e152ad38254f9edae474ed86b7c7
Dream2000i <121653106+Dream2000i@users.noreply.github.com>
2023-04-06 20:14:38 +0300

    add Leaderboard component (#12)
    * add Leaderboard component

--- packages/client/src/features/Leaderboard/Leaderboard.tsx
+++ packages/client/src/features/Leaderboard/Leaderboard.tsx
@@ -1,5 +1,54 @@
 import type { FC } from 'react';
+import { useIntl } from 'react-intl';
+import { Typography } from 'antd';
+import { CrownTwoTone } from '@ant-design/icons';
+import { messages, data } from './common';
 
 import './Leaderboard.scss';
 
-export const Leaderboard: FC = () => <div>Leaderboard</div>;
+const { Title } = Typography;
+
+type LeaderboardPayer = {
+  place: number;
+  nick: string;
+  profit: number;
+  gameTime: string;
+};
+
+export const Leaderboard: FC = () => {
+  const { formatMessage: fm } = useIntl();
+  const leaderboardList: Array<LeaderboardPayer> | [] = data ?? [];
+
+  return (
+    <div className="leaderboard">
+      <Title level={2} className="leaderboard_title">
+        {fm(messages.title)}
+      </Title>
+      <div className="leaderboard_players">
+        {leaderboardList &&
+          leaderboardList.map((item) => (
+            <div className="player" key={item.nick}>
+              <div className="player-content">
+                <div className="place">
+                  <Title level={2} className="place-title">
+                    {item.place === 1 ? <CrownTwoTone /> : item.place}
+                  </Title>
+                </div>
+                <div className="nick">
+                  <Title level={3} className="nick-title">
+                    {item.nick}
+                  </Title>
+                </div>
+                <div className="profit">
+                  {item.profit} {fm(messages.profit)}
+                </div>
+                <div className="game-time">
+                  {item.gameTime} {fm(messages.daysInGame)}
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
