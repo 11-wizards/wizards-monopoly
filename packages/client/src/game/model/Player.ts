a7ffb7da811a6f842cd91098a5db26d4a425b635
Pavel Bologan <p.bologan@gmail.com>
2023-04-13 23:37:02 +0400

    refactors Player class

--- packages/client/src/game/model/Player.ts
+++ packages/client/src/game/model/Player.ts
@@ -1,17 +1,25 @@
+import type { Property } from './Property';
+
 export class Player {
-  private readonly id: number;
+  private readonly id: string;
   private readonly name: string;
   private readonly color: string;
-  private currentCardId: number;
+  private x: number;
+  private y: number;
+  private amount: number;
+  private property: Property[];
 
-  constructor(id: number, name: string, color: string) {
+  constructor(id: string, name: string, color: string) {
     this.id = id;
     this.name = name;
     this.color = color;
-    this.currentCardId = 0;
+    this.x = 0;
+    this.y = 0;
+    this.amount = 15_000_000;
+    this.property = [];
   }
 
-  public getId(): number {
+  public getId(): string {
     return this.id;
   }
 
@@ -23,11 +31,59 @@ export class Player {
     return this.color;
   }
 
-  public getCurrentCardId(): number {
-    return this.currentCardId;
+  public getX(): number {
+    return this.x;
   }
 
-  public setCurrentCardId(cardId: number) {
-    this.currentCardId = cardId;
+  public setX(x: number) {
+    this.x = x;
+  }
+
+  public getY(): number {
+    return this.y;
+  }
+
+  public setY(y: number) {
+    this.y = y;
+  }
+
+  public getAmount(): number {
+    return this.amount;
+  }
+
+  public sellProperty(propertyId: string, amount: number): Nullable<Property> {
+    const found = this.property.find((property) => property.getId() === propertyId);
+    if (found === undefined) {
+      return null;
+    }
+    this.amount += amount;
+
+    return found;
+  }
+
+  public buyProperty(property: Property): Nullable<number> {
+    if (this.amount >= property.getPrice()) {
+      this.amount -= property.getPrice();
+      this.property.push(property);
+
+      return property.getPrice();
+    }
+
+    return null;
+  }
+
+  public pay(amount: number): boolean {
+    this.amount -= amount;
+    if (this.amount < 0) {
+      this.amount = 0;
+
+      return false;
+    }
+
+    return true;
+  }
+
+  public chargeFee(amount: number) {
+    this.amount += amount;
   }
 }
