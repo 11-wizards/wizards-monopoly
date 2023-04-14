d181b96b381df1c3dc0c15c2b5f8566f87a39890
Pavel Bologan <p.bologan@gmail.com>
2023-04-14 20:03:11 +0400

    refactors gameSlice

--- packages/client/src/app/slices/gameSlice.ts
+++ packages/client/src/app/slices/gameSlice.ts
@@ -1,23 +1,22 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+import type { PlayerColor } from 'types/enums/main';
+import { PlayerDirection } from 'types/enums/main';
+import { Player } from 'game/model';
 import type { GameSetupFormData } from 'features/GameSetup/types';
-import type { PlayerColors } from 'types/enums/main';
+import type { PlayerPosition } from 'models/game.model';
+import type { RootState } from '../store';
 
 type GameState = {
   numberOfPlayers: number;
-  players: Players;
+  players: Player[];
+  playersPositions: PlayerPosition[];
 };
-
-type Player = {
-  color: PlayerColors;
-  name: string;
-};
-
-type Players = Record<string, Player>;
 
 const initialState: GameState = {
   numberOfPlayers: 0,
-  players: {},
+  players: [],
+  playersPositions: [],
 };
 
 export const gameSlice = createSlice({
@@ -25,27 +24,48 @@ export const gameSlice = createSlice({
   initialState,
   reducers: {
     definePlayers: {
-      reducer: (state, action: PayloadAction<Players>) => {
-        state.players = action.payload;
+      reducer: (
+        state,
+        action: PayloadAction<{ players: Player[]; playersPositions: PlayerPosition[] }>,
+      ) => {
+        const { players, playersPositions } = action.payload;
+        state.players = players;
+        state.playersPositions = playersPositions;
       },
-      prepare: (players: GameSetupFormData) => ({
-        payload: Object.keys(players).reduce((result: Players, key): Players => {
-          const [keyName, playerNum] = key.split('_').slice(1);
-          const playerKey = `player_${playerNum}`;
-          const player = result[playerKey] || {};
+      prepare: (formPlayers: GameSetupFormData) => {
+        const keys = Object.keys(formPlayers);
+        const playersCount = keys.length / 2;
+        const playersObject: Record<number, { name: string; color: PlayerColor }> = {};
 
-          return {
-            ...result,
-            [playerKey]: {
-              ...player,
-              [keyName]: players[key],
-            },
-          };
-        }, {}),
-      }),
+        keys.forEach((key) => {
+          const splitKey = key.split('_');
+          const keyNum = Number(splitKey[splitKey.length - 1]);
+          const propertyName = splitKey[splitKey.length - 2];
+          const value = formPlayers[key];
+          if (propertyName === 'name' || propertyName === 'color') {
+            playersObject[keyNum] = { ...playersObject[keyNum], [propertyName]: value };
+          }
+        });
+
+        const players: Player[] = [];
+        const playersPositions: PlayerPosition[] = [];
+
+        for (let i = 1; i <= playersCount; i++) {
+          const { name, color } = playersObject[i];
+          const y = (i - 1) * 20;
+          const id = nanoid();
+          const position: PlayerPosition = { id, x: 0, y, direction: PlayerDirection.RIGHT };
+          players.push(new Player(id, name, color));
+          playersPositions.push(position);
+        }
+
+        return { payload: { players, playersPositions } };
+      },
     },
   },
 });
+
+export const selectPlayers = (rootState: RootState) => rootState.game.players;
 
 export const { definePlayers } = gameSlice.actions;
 
