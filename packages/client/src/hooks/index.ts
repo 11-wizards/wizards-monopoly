793f8becba542ec9ba3fb1a8c0fa126a7f3f27d3
Dream2000i <121653106+Dream2000i@users.noreply.github.com>
2023-04-15 18:55:10 +0300

    feature/WIZ-22 (#13)
    * add component Forum

---
+++ packages/client/src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useFetchPosts } from './useFetchPosts';
+export { useFetchTopics } from './useFetchTopics';
+export { useFetchUsers } from './useFetchUsers';
+export { useAppDispatch, useAppSelector } from './redux';
---
+++ packages/client/src/hooks/useFetchUsers.tsx
@@ -0,0 +1,24 @@
+import type { Author } from 'models/forum.model';
+import type React from 'react';
+import { useEffect, useState } from 'react';
+import { forumApi } from 'api/forum.api';
+import { isArray } from 'helpers';
+
+export const useFetchUsers = (): [
+  Array<Author>,
+  React.Dispatch<React.SetStateAction<Array<Author>>>,
+] => {
+  const [state, setState] = useState<Array<Author>>([]);
+
+  useEffect(() => {
+    forumApi
+      .getAuthor()
+      .then(({ data = [] }: { data: [] | Array<Author> }) => {
+        if (isArray(data)) {
+          setState(data);
+        }
+      })
+      .catch(() => {});
+  }, []);
+  return [state, setState];
+};
