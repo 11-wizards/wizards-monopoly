254d897689993bcc516ed5853023d69039f6214f
Dream2000i <121653106+Dream2000i@users.noreply.github.com>
2023-04-15 18:51:05 +0300

    fix

--- packages/client/src/features/Forum/common.ts
+++ packages/client/src/features/Forum/common.ts
@@ -5,7 +5,7 @@ export const messages = defineMessages({
   createTopicBtn: { id: 'forum.btn.create-theme', defaultMessage: 'Create a new theme' },
   responseBtn: { id: 'forum.btn.response', defaultMessage: 'user response' },
   changeTopic: { id: 'forum.topics.change', defaultMessage: 'Choose a theme' },
-  topicLabel: { id: 'forum.topics.theme', defaultMessage: 'Theme:' },
+  topicLabel: { id: 'forum.topics.topic', defaultMessage: 'Theme:' },
   authorLabel: { id: 'forum.topics.author', defaultMessage: 'Author:' },
   responsePost: { id: 'forum.posts.response', defaultMessage: 'Reply in the theme' },
 });
