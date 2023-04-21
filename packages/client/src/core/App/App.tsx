b0d11fc0820171ebefb479d731607035f431f95e
Anton Rodionov <stvdent47@gmail.com>
2023-04-21 13:17:23 +0400

    WIZ-23: add global l10n

--- packages/client/src/core/App/App.tsx
+++ packages/client/src/core/App/App.tsx
@@ -1,10 +1,10 @@
-import { type FC, useEffect, useState } from 'react';
+import { type FC, useEffect } from 'react';
 import { IntlProvider } from 'react-intl';
-import { GlobalOutlined } from '@ant-design/icons';
 import { fetchCurrentUser } from 'app/slices/userSlice';
 import { LOCAL_STORAGE_IS_AUTH_KEY } from 'constants/localStorage';
 import { Router } from 'core/Router';
 import { useAppDispatch } from 'hooks/redux';
+import { useLocale } from 'hooks/useLocale';
 import { en, ru } from 'translations';
 
 import 'styles/main.scss';
@@ -12,11 +12,7 @@ import './App.scss';
 
 export const App: FC = () => {
   const dispatch = useAppDispatch();
-  const [locale, setLocale] = useState(navigator.language.slice(0, 2) || 'en');
-
-  const toggleLocale = () => {
-    setLocale((prevState) => (prevState === 'en' ? 'ru' : 'en'));
-  };
+  const [locale] = useLocale();
 
   useEffect(() => {
     if (localStorage.getItem(LOCAL_STORAGE_IS_AUTH_KEY)) {
@@ -31,8 +27,6 @@ export const App: FC = () => {
 
   return (
     <div className="app">
-      {/* временное решение до внедрения навбара */}
-      <GlobalOutlined className="app__languageToggle" onClick={toggleLocale} />
       <IntlProvider locale={locale} messages={locale === 'en' ? en : ru}>
         <Router />
       </IntlProvider>
---
+++ packages/client/src/hooks/useLocale.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { setLocale } from 'app/slices/localeSlice';
+import { LOCAL_STORAGE_IS_AUTH_KEY } from 'constants/localStorage';
+import { useAppDispatch, useAppSelector } from './redux';
+
+export function useLocale() {
+  const dispatch = useAppDispatch();
+  const locale = useAppSelector((state) => state.locale);
+
+  useEffect(() => {
+    let initialLocale =
+      localStorage.getItem(LOCAL_STORAGE_IS_AUTH_KEY) || navigator.language.slice(0, 2);
+
+    if (!initialLocale || !['en', 'ru'].includes(initialLocale)) {
+      initialLocale = 'en';
+    }
+
+    dispatch(setLocale(initialLocale as LocaleType));
+  }, [dispatch]);
+
+  const toggleLocale = () => {
+    const nextLocale = locale === 'en' ? 'ru' : 'en';
+
+    localStorage.setItem(LOCAL_STORAGE_IS_AUTH_KEY, nextLocale);
+    dispatch(setLocale(nextLocale));
+  };
+
+  return [locale, toggleLocale] as const;
+}
