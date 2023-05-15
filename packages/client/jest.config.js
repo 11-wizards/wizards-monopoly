909821d36d36c2e6b4b9f1fac20ec0c24ea61e2c
Pavel Bologan <p.bologan@gmail.com>
2023-05-15 14:14:01 +0400

    fixes lint error

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 dotenv.config();
 
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
